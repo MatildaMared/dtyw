0c48586fde3a460075d7e937f8c7923cbab5188e
Matilda Mared <matildamared@live.se>
2022-07-28 07:33:17 +0200

    add image and logo to header component

---
+++ src/lib/header/Logo.svelte
@@ -0,0 +1,21 @@
+<script lang="ts"></script>
+
+<p class="logo">
+	Do things <span class="emphasized">your</span> way
+</p>
+
+<style lang="scss">
+	.logo {
+		font-family: var(--font-secondary);
+		color: var(--color-white);
+		margin-bottom: var(--spacing-large);
+		line-height: 1;
+		font-size: var(--font-size-xl);
+		text-transform: uppercase;
+
+		& .emphasized {
+			font-weight: 700;
+			font-style: italic;
+		}
+	}
+</style>

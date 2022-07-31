1fdd3fc13ca08fa78ee3eec7716ef2dcfb7c4c69
Matilda Mared <matildamared@live.se>
2022-07-31 19:04:07 +0200

    add card component

---
+++ src/lib/card/Card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let imgUrl: string;
+	export let altText: string;
+	export let heading: string;
+</script>
+
+<li>
+	<img src={imgUrl} alt={altText} />
+	<h3>{heading}</h3>
+	<p>
+        <slot />
+    </p>
+</li>
+
+<style lang="scss">
+    li {
+        width: 100%;
+        max-width: 350px;
+        border: 1px solid blue;
+    }
+</style>

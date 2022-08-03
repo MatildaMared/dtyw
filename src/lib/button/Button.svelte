fbf46a94fde100baa2972b05d809b3cbda8dc5e8
Matilda Mared <matildamared@live.se>
2022-08-03 08:18:11 +0200

    change color of button

--- src/lib/button/Button.svelte
+++ src/lib/button/Button.svelte
@@ -14,13 +14,13 @@
 		box-shadow: 8px 4px 8px 0 rgba(0, 0, 0, 0.2);
 		transition: all 0.2s;
 		cursor: pointer;
-		background-color: var(--color-secondary-dark);
+		background-color: var(--color-secondary);
 		color: var(--color-white);
 		letter-spacing: 0.5px;
 
 		&:hover {
-			background-color: var(--color-secondary);
-			box-shadow: 8px 8px 16px 0 rgba(0, 0, 0, 0.2);
+			background-color: var(--color-secondary-dark);
+			box-shadow: 8px 8px 10px 0 rgba(0, 0, 0, 0.2);
 			transform: translateY(-2px);
 		}
 	}

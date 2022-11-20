96ac07d4984a8fd039a08fa3bbea74142163566d
Matilda Mared <matildamared@live.se>
2022-11-20 14:20:44 +0100

    add integritetspolicy page

---
+++ src/routes/integritetspolicy/index.svelte
@@ -0,0 +1,123 @@
+<script>
+</script>
+
+<main>
+	<h1>Information till kunder om Do Things Your Ways behandling av personuppgifter</h1>
+
+	<h2>Do Things Your Way värnar om att skydda din personliga integritet</h2>
+	<p>
+		Do Things Your Way, org. nr 900306-0531 värnar om att skydda din personliga integritet. Vi
+		arbetar därför aktivt med att skydda dina personuppgifter på bästa sätt i enlighet med denna
+		personuppgiftspolicy och gällande lagstiftning.
+	</p>
+
+	<h2>Vem är personuppgiftsansvarig?</h2>
+	<p>
+		Markus Mattsson, 900306-0531, med enskild firma Do Things Your Way, org. nr 900306-0531 är
+		personuppgiftsansvarig för behandlingen av dina personuppgifter som beskrivs i denna
+		informationstext. Do Things Your Way benämns fortsättningsvis som ”Do Things Your Way”, ”vi”
+		eller ”oss”.
+	</p>
+
+	<h2>Vilka personuppgifter behandlar vi?</h2>
+	<p>
+		Vi behandlar de uppgifter som du lämnar till oss, exempelvis vid anmälan till ett kostnadsfritt
+		webbinarie eller vid köp av våra tjänster. De personuppgifter vi behandlar är exempelvis: namn
+		och personnummer, adress, telefonnummer, e-postadress, användarnamn, köp- och
+		betalningsinformation, bild, video- eller ljudupptagning, och uppgifter om hälsa
+	</p>
+
+	<h2>Särskilt om uppgifter om hälsa</h2>
+	<p>
+		Do Things Your Way behandlar enbart uppgifter om hälsa om du uttryckligen har samtyckt till
+		detta, och endast för de ändamål du har godkänt. Du kan alltid återkalla ditt samtycke genom att
+		meddela Do Things Your Way att du inte längre samtycker till behandlingen av hälsouppgifter (se
+		kontaktuppgifter till oss nedan).
+	</p>
+
+	<h2>Särskilt om bild, video- eller ljudupptagningar</h2>
+	<p>
+		Samtal som du har med våra medarbetare eller konsulter kan komma att spelas in med video- eller
+		ljudupptagning. Detta kan ske för att utveckla vår verksamhet (exempelvis i utbildningsändamål)
+		och för att tillhandahålla kundservice. Video- eller ljudupptagningar avsedda att utveckla vår
+		verksamhet och tillhandahålla kundservice sparas som längst i 12 månader.
+	</p>
+
+	<h2>Varför behandlar vi dina personuppgifter?</h2>
+	<p>
+		Det är en förutsättning för att vi ska kunna ingå avtal med dig som kund att du lämnar dina
+		personuppgifter till oss. Nedan anges i tabellform för vilka ändamål Do Things Your Way
+		behandlar dina personuppgifter och vilka rättsliga grunder Do Things Your Way har för behandling
+		av dina personuppgifter.
+	</p>
+
+	<img src="images/integritetspolicy.webp" alt="Integritetspolicy" />
+
+	<h2>Med vilka delar Do Things Your Way dina personuppgifter?</h2>
+	<p>
+		Do Things Your Way kan komma att lämna ut dina personuppgifter till våra konsulter,
+		samarbetsparternas, våra leverantörer av IT-tjänster och datasystem samt finansiella och
+		juridiska tjänster, branschorganisationer, försäkringsbolag, banker, delgivningsman och till
+		myndigheter och andra offentliga organ. Do Things Your Way behandlar som regel inte dina
+		personuppgifter utanför EU/EES. I den mån behandling av dina personuppgifter sker utanför EU/EES
+		sker sådan behandling alltid enligt gällande lag och med iakttagande av föreskrivna
+		skyddsregler.
+	</p>
+
+	<h2>Hur länge behandlar vi dina personuppgifter?</h2>
+	<p>
+		Vi behandlar som utgångspunkt dina personuppgifter under den tid som avtalsförhållandet består
+		och som längst i 3 år därefter. Vissa personuppgifter kan sparas under en längre tid beroende på
+		varför personuppgifterna behandlas. De personuppgifter som behandlas för att kunna sköta vår
+		ekonomi och bokföring enligt lag sparas i sju år. Vi kan spara dina personuppgifter i upp till
+		elva år (eller under den tid som tvist pågår) för att kunna fastställa, göra gällande eller
+		försvara oss mot rättsliga anspråk. För att kunna fastställa, göra gällande eller försvara oss
+		mot miljörättsliga anspråk kan vi behandla dina personuppgifter under en längre tid.
+	</p>
+
+	<h2>Vad har du för rättigheter?</h2>
+	<p>
+		Du har rätt att få tillgång till dina personuppgifter som Do Things Your Way behandlar. Du har
+		också rätt att begära radering, rättelse eller begränsning av behandlingen av dina
+		personuppgifter, att invända mot Do Things Your Ways behandling av dina personuppgifter och att
+		få ut dina personuppgifter i ett strukturerat, allmänt använt och maskinläsbart format för
+		överföring till annan leverantör (s.k. dataportabilitet). Kontakta Do Things Your Way om du vill
+		utnyttja någon av dessa rättigheter. Du har alltid rätt att få närmare information om
+		personuppgiftsbehandling som sker med stöd av en s.k. intresseavvägning (se nedan) samt att
+		invända mot sådan behandling. Du har också alltid rätt att avregistrera dig från
+		direktmarknadsföring. Om du har synpunkter på Do Things Your Ways behandling av dina
+		personuppgifter kan du lämna in ett klagomål till Integritetsskyddsmyndigheten.
+	</p>
+
+	<h2>Hör av dig om du har frågor</h2>
+	<p>
+		Om du har frågor eller synpunkter på Do Things Your Ways behandling av dina personuppgifter,
+		tveka inte höra av dig till Do Things Your Way via post, Do Things Your Way, Kaptensgatan 10E,
+		30245, Halmstad eller via e-post på markus@dothingsyourway.com
+	</p>
+</main>
+
+<style lang="scss">
+	@use "../../styles/breakpoints.scss" as *;
+
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+		max-width: 800px;
+		margin: var(--spacing-xl) auto;
+		padding: 0 16px;
+		gap: var(--spacing-s);
+
+		h1 {
+			text-align: center;
+			font-size: var(--font-size-xl);
+			margin-bottom: var(--spacing-m);
+		}
+
+		h2 {
+			width: 100%;
+		}
+	}
+</style>

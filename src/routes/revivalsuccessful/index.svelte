8f0c9a76269ba8ea0a0c6a7f4685480c2cf82a02
Markus Mattsson <markusmattsson90@gmail.com>
2023-03-23 15:15:18 +0100

    Add revivalsuccessful

---
+++ src/routes/revivalsuccessful/index.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Divider from "$lib/divider/Divider.svelte";
+	import AboutMarkus from "$lib/homepage/about-markus/AboutMarkus.svelte";
+
+</script>
+
+<main>
+	<h1>Grattis! 🎉</h1>
+	<h2>Du är kvar på maillistan och fortsätter få info och inspiration om stress och företagande</h2>
+</main>
+<AboutMarkus/>
+
+<style lang="scss">
+	@use "../../styles/breakpoints.scss" as *;
+
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		max-width: 800px;
+		margin: var(--spacing-xl) auto;
+		padding: 0 100px;
+		gap: var(--spacing-s);
+
+		h1 {
+			text-align: center;
+			font-size: var(--font-size-xl);
+			margin-bottom: var(--spacing-m);
+		}
+
+		h2 {
+			width: 100%;
+			text-align: center;
+		}
+	}
+</style>

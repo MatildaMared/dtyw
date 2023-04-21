02437a300517890293c5abbb50a5995637e7961b
Markus Mattsson <markusmattsson90@gmail.com>
2023-04-21 10:05:39 +0200

    add email video

--- src/routes/email/vagledningssamtal/21april/index.svelte
+++ src/routes/email/vagledningssamtal/21april/index.svelte
@@ -1,27 +1,17 @@
 <script>
 	import Divider from "$lib/divider/Divider.svelte";
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		const el = document.getElementById('hiddenButton');
-		el.style.visibility = "hidden";
-
-		setTimeout(() => {
-			el.style.visibility = 'visible';
-		}, 300000); 
-	});
 
 </script>
 
 <main>
-	<h1>Tre råd som förändrade mitt liv och mitt företag</h1>
+	<h1>Stress är inte problemet</h1>
 	<div
 		id="Container"
 		style="padding-bottom:56.25%; position:relative; display:block; width: 100%"
 	>
 		<iframe
 			id="UstreamIframe"
-			src="https://player.vimeo.com/video/787887511?h=9ca390475c&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+			src="https://player.vimeo.com/video/819762498?h=acaddad7ee&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
 			width="100%" height="100%"
 			style="position:absolute; top:0; left: 0"
 			allowfullscreen
@@ -32,14 +22,14 @@
 		</iframe>
 	</div>
 
-	<a href="https://calendly.com/markusmattsson/strategisamtal?utm_source=homepage&utm_medium=email&utm_campaign=treradsomforandrademittlivochmittforetag" 
-		class="button" id="hiddenButton" target="_blank">
-		Boka ditt gratis samtal nu!
+	<a href="https://calendly.com/markusmattsson/vagledning?utm_source=homepage&utm_medium=email&utm_campaign=stressarinteproblemet-21april" 
+		class="button" target="_blank">
+		Boka ditt kostnadsfria samtal nu!
 	</a>
 </main>
 
 <style lang="scss">
-	@use "../../styles/breakpoints.scss" as *;
+	@use "../../../../styles/breakpoints.scss" as *;
 
 	main {
 		display: flex;
